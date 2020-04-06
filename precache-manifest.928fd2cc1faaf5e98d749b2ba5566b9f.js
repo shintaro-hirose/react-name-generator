92639e73a0ddb33b5cfbfaa96a053a05cc4b0808
shintaro-hirose <joeleshin@eis.hokudai.ac.jp>
2020-04-06 23:11:01 +0900

    Updates

---
+++ precache-manifest.928fd2cc1faaf5e98d749b2ba5566b9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "964d671f1de797c2823c7a3026e0d4f9",
+    "url": "/react-name-generator/index.html"
+  },
+  {
+    "revision": "1224236fba326d747eff",
+    "url": "/react-name-generator/static/css/main.f0c8ca91.chunk.css"
+  },
+  {
+    "revision": "bdc11f852bd1de3bc071",
+    "url": "/react-name-generator/static/js/2.159035d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-name-generator/static/js/2.159035d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1224236fba326d747eff",
+    "url": "/react-name-generator/static/js/main.b53199c6.chunk.js"
+  },
+  {
+    "revision": "6d9ad55636a8b050fb2b",
+    "url": "/react-name-generator/static/js/runtime-main.2ac0965c.js"
+  },
+  {
+    "revision": "79cb9c3553f49895d0c7681ab112fd28",
+    "url": "/react-name-generator/static/media/react-name-logo.79cb9c35.svg"
+  }
+]);
